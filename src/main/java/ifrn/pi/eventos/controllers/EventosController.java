ec9464b4e7e0b9618eb589710b86541036afa29f
Graziele Fernandes de Oliveira <20211204010046@ifrn.local>
2024-10-23 13:24:16 -0300

    Criando a página de formulário para cadastro do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,14 +1,29 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
-	
+
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+
+	@PostMapping("/eventos/salvar")
+	public String salvar(Evento evento) {
+		System.out.println("Evento salvo com os seguintes dados");
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+		return "ExecutandoSubmit";
 	}
 
 }
