5360a49165f0d96c2dd5c74367a90f208d6950c0
Graziele Fernandes de Oliveira <20211204010046@ifrn.local>
2024-10-30 16:03:33 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,28 +1,33 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 	
 	@Autowired
 	private EventoRepository er; 
 
-	@RequestMapping("/eventos/form")
+	@RequestMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 	}
 
-	@RequestMapping("/eventos/submit")
+	@PostMapping
 	public String submit(Evento evento) {
 		System.out.println("Evento salvo com os seguintes dados");
 		System.out.println(evento.getNome());
@@ -31,6 +36,15 @@ public class EventosController {
 		System.out.println(evento.getHorario());
 		er.save(evento);
 		return "eventos/ExecutandoSubmit";
+	}
+	
+	@GetMapping
+	public ModelAndView listar() {
+		
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 
 }
